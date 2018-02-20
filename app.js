f3838db12aa5b79be9b8e41599e644e35b14e5b8
cara ottmar <cara.ottmar@gmail.com>
2018-02-19 21:09:40 -0800

    I have 3 images appearing randomly, side by side.

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 //data: global vars for DOM
-//      contructor
-//      instances
 //      total clicks <25
 //      array
 
@@ -8,12 +6,18 @@
 
 //array to store the ojbects
 var productNames = ['banana', 'boots', 'breakfast', 'bubblegum', 'chair', 'dog-duck', 'dragon', 'greenmonster', 'pen', 'pet-sweep', 'r2d2bag', 'scissors', 'shark', 'sweep', 'tauntaun', 'toiletstand', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+
 ProductImage.allProductImages = [];
+var img1 = document.getElementById('img-1');
+var img2 = document.getElementById('img-2');
+var img3 = document.getElementById('img-3');
 
 //make an object
 function ProductImage(imageName) {
   this.imageName = imageName;
   this.filepath = 'img/' + imageName + '.jpg';
+  this.pickClickCount = 0;
+  this.numPickClick = 0;
   ProductImage.allProductImages.push(this);
 }
 
@@ -22,16 +26,39 @@ for (var i = 0; i < productNames.length; i++) {
   new ProductImage(productNames[i]);
 }
 
+
+
 //write a function to randomly display images
 function randomImage() {
-  var randomImage = Math.floor(Math.random() * ProductImage.allProductImages.length);
-  console.log(ProductImage.allProductImages[randomImage]);
+  var randomIndex = [];
+  for (i = 0; i < 3; i++) {
+    var randomPick = Math.floor(Math.random() * ProductImage.allProductImages.length);
+    randomIndex.push(randomPick);
+  }
+  console.log(randomIndex);
+
+  img1.src= ProductImage.allProductImages[randomIndex[0]].filepath;
+  img1.alt= ProductImage.allProductImages[randomIndex[0]].imageName;
+  img1.title= ProductImage.allProductImages[randomIndex[0]].imageName;
+
+  img2.src= ProductImage.allProductImages[randomIndex[1]].filepath;
+  img2.alt= ProductImage.allProductImages[randomIndex[1]].imageName;
+  img2.title= ProductImage.allProductImages[randomIndex[1]].imageName;
+
+  img3.src= ProductImage.allProductImages[randomIndex[2]].filepath;
+  img3.alt= ProductImage.allProductImages[randomIndex[2]].imageName;
+  img3.title= ProductImage.allProductImages[randomIndex[2]].imageName;
 }
 randomImage();
-//we need to access the img element in the dom
 
-//console.log thefunction --- refer to demo
+img1.addEventListener('click', randomImage);
+img2.addEventListener('click', randomImage);
+img3.addEventListener('click', randomImage);
 
-//assign the src, alt, and title attributes to the img element
+//write a function that will generate a random number between 0 and 19 -- needs to give 3 different numbers between
 
-//make sure we have a listener event for lick and on what image
+
+// randomImage(document.getElementById('img-1'))[0];
+// for (i = 0; i < ProductImage.allProductImages.length; i++) {
+//   ProductImage.allProductImages.splice(Math.floor(Math.random() * ProductImage.allProductImages.length), 1)[0];
+// }
--- user_stories.md
+++ user_stories.md
@@ -1,6 +1,17 @@
 // user stories //
 
-Developer: As a developer, I want to create a site thats stores, calculates, and visually displays the data of user selections. The selections will be made from a choice of three images each round. Once image is selected, three new images will be displayed that do not show any image in the previous round nor the image that was selected in the previous round. Once the user has made 25 choices (or clicks) that data will be represented to them.
+edit my user stories to be a better step by step to hopefully lead me to my next move
+
+Developer:
+1. As a developer, I want to create a site that displays product images
+2. Put three images in a row, with each image clickable
+3. The user's favorite image will be clicked, causing a new round of images to be displayed
+  3a. the images cannot be any of the previous images and cannot be the previously selected favorite
+4. The number of rounds should be 25
+5. After the 25th click, the number of times a picture was clicked needs to be counted
+ 
+6. The calculated data needs to be stored
+
 
 Marketer: As a markerting researcher, I want to see what users would most like to buy from a new catalogue on public transit called BusMall. I will give the developer all of the product images that I want displayed to the user to make choices from. I want the user to have a selection of three images and to choose which they'd prefer to purchase from BusMall. After the user has made their choices, I want the user to see a wrap-up of their choices displayed to them. 
 
